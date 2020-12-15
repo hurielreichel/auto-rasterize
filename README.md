7878d6f875a6a1a6dacdd99f84dbac7767d988b3
hurielreichel <huriel.ruan@gmail.com>
2020-12-15 17:14:51 +0100

    New README and code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# auto-rasterize
-This tool allows one to rasterize a shapefile, automatically turning it into a binary GeoTiff file
+# Overview
+This tool allows one to rasterize a shapefile, automatically turning it into a binary GeoTiff file. This is usefull when creating ground truth dataset for image segmentation and/or object detection. The thought use case was transforming polygons of the houses in Switzerland into raster with a croping argument.
+
+## auto-rasterize
+In order to use this code, the main arguments are input shapefile path, output GeoTiff (.tif) file path and crop arguments. Of those, you'll have minimum X (longitude), minimum Y (latitude), maximum X and maximum Y. At last, the pixel size shall be defined as well. 
+
+The following exemplifies the code usage in a linux terminal in the CH1903+ Swiss Coordinate System.
+
+```
+$python3 auto_rasterize.py -i /path/to/shapefile.shp -o /path/to/output/GeoTiff.tif -xmin 2709756 --ymin 1268735 --xmax 2711300 --ymax 1270009 -p 10
+```
+
+The figure below demonstrates a polygon of houses in Frauenfeld (TG - Switzerland) - image on the left -  being rasterized and croped to a specific extent - image on the right.
+
+![](example.png)
+
+# Copyright and License
+
+las-to-uv3 - Huriel Reichel Nils Hamel
+Copyright (c) 2020 Republic and Canton of Geneva
+
+This program is licensed under the terms of the GNU GPLv3. Documentation and illustrations are licensed under the terms of the CC BY-NC-SA.
+
---
+++ auto_rasterize.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+#  auto_rasterize
+#
+#     Huriel Reichel - huriel.ruan@gmail.com
+#     Nils Hamel - nils.hamel@bluewin.ch
+#     Copyright (c) 2020 Republic and Canton of Geneva
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gdal
+import ogr
+import argparse
+
+pm_argparse = argparse.ArgumentParser()
+
+# argument and parameter directive #
+pm_argparse.add_argument( '-i', '--input', type=str  , help='shapefile path'    )
+pm_argparse.add_argument( '-o', '--output' , type=str  , help='GeoTiff output path' )
+pm_argparse.add_argument( '--xmin' , type=float  , help='minimum X / Longitude value' )
+pm_argparse.add_argument( '--ymin' , type=float  , help='minimum Y / Latitude value' )
+pm_argparse.add_argument( '--xmax' , type=float  , help='maximum X / Longitude value' )
+pm_argparse.add_argument( '--ymax' , type=float  , help='minimum Y / Latitude value' )
+pm_argparse.add_argument( '-p' , '--pixel', type=float  , help='output pixel size' )
+
+# read argument and parameters #
+pm_args = pm_argparse.parse_args()    
+
+
+def rasterize(input, output, xmin, ymin, xmax, ymax, pixel):
+    # Open the data source
+    orig_data_source = ogr.Open(input)
+    # Make a copy of the layer's data source because we'll need to 
+    # modify its attributes table
+    source_ds = ogr.GetDriverByName("Memory").CopyDataSource(
+            orig_data_source, "")
+    source_layer = source_ds.GetLayer(0)
+    source_srs = source_layer.GetSpatialRef() 
+    
+    # Create the destination data source
+    x_res = int((xmax - xmin) / pixel)
+    y_res = int((ymax - ymin) / pixel)
+    target_ds = gdal.GetDriverByName('GTiff').Create(output, x_res,
+            y_res, 3, gdal.GDT_Byte)
+    target_ds.SetGeoTransform((
+            xmin, pixel, 0,
+            ymax, 0, -pixel,
+        ))
+    if source_srs:
+        # Make the target raster have the same projection as the source
+        target_ds.SetProjection(source_srs.ExportToWkt())
+    else:
+        # Source has no projection (needs GDAL >= 1.7.0 to work)
+        target_ds.SetProjection('LOCAL_CS["arbitrary"]')
+    # Rasterize
+    err = gdal.RasterizeLayer(target_ds, (3, 2, 1), source_layer,
+            burn_values=(1, 1, 1))
+    if err != 0:
+        raise Exception("error rasterizing layer: %s" % err)
+        
+rasterize(pm_args.input, pm_args.output, pm_args.xmin, pm_args.ymin, pm_args.xmax, pm_args.ymax, pm_args.pixel)
